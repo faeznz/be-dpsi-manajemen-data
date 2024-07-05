f5be2f50998813c1b7b2a3b36c13ae65d094b170
Zulfan Faizun Najib <121880142+faeznz@users.noreply.github.com>
2024-07-05 18:58:57 +0700

    fix vercel

--- app.js
+++ app.js
@@ -27,10 +27,6 @@ mongoose.connection.on('error', (err) => {
   console.error(`MongoDB connection error: ${err}`);
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
