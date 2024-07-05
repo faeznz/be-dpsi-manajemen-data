caa193e6f5c2a729d5805ce464cbb4e587c40d2d
Zulfan Faizun Najib <121880142+faeznz@users.noreply.github.com>
2024-07-05 19:38:11 +0700

    fix index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('<html><body><h1>API Final Project DPSI</h1><p>by Zulfan Faizun Najib</p></body></html>');
 });
 
 module.exports = router;
